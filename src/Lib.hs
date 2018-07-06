c7c391b301f38441ed864230931a216580ad8d62
Yuan Wang <yuan.wang@workiva.com>
2018-07-06 15:25:51 -0600

    sort query string

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,9 +9,11 @@ import qualified Data.ByteString.Base16    as B16 (encode)
 import qualified Data.ByteString.Char8     as C
 import           Data.CaseInsensitive      (original)
 import           Data.Char                 (toLower)
-import           Data.List                 (intersperse, lines, sortBy)
+import           Data.List                 (intersperse, lines, sortBy, sortOn)
+import           Data.Maybe
 import           Data.Monoid               ((<>))
 import           Network.HTTP.Client
+import           Network.HTTP.Simple
 import           Network.HTTP.Types
 import           Network.HTTP.Types.Header
 import           Network.HTTP.Types.Method
@@ -23,13 +25,24 @@ canonicalHeaders :: Request -> ByteString
 canonicalHeaders req = C.concat $ map (\(hn,hv) -> bsToLower (original hn) <> ":" <> hv <> "n") hs
     where hs = headers req
 
+isChunckRequestBody :: RequestBody -> Bool
+isChunckRequestBody (RequestBodyLBS _)       = False
+isChunckRequestBody (RequestBodyBS _)        = False
+isChunckRequestBody (RequestBodyBuilder _ _) = False
+isChunckRequestBody _                        = True
+
+isChunckRequest :: Request -> Bool
+isChunckRequest = isChunckRequestBody . requestBody
+
+canonicalQueryString :: Request -> ByteString
+canonicalQueryString = renderQuery False . sortOn fst . parseQuery . queryString
 
 canonicalRequest :: Request -> ByteString -> ByteString
 canonicalRequest req body = C.concat $
-        intersperse "n"
+        intersperse "\n"
             [ method req
             , path req
-            , queryString req
+            , canonicalQueryString req
             , canonicalHeaders req
             , signedHeaders req
             , hexHash body
