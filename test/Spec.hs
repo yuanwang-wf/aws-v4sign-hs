3de6e920e4b73ad0ecb2b831c5b736d3d70ec47d
Yuan Wang <yuan.w0811@gmail.com>
2018-10-20 20:54:24 -0600

    first pass

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,9 +3,26 @@
 import qualified   Data.ByteString as BS
 import             Data.Maybe
 import             Lib
+import           Network.HTTP.Client
+import           Network.HTTP.Simple
+import           Network.HTTP.Types
+import           Network.HTTP.Types.Header
+import           Network.HTTP.Types.Method
 import             Test.Hspec
 
 
+targetRequestM :: Maybe Request
+targetRequestM = do
+    initReq <- parseUrlThrow "https://iam.amazonaws.com"
+    let req = setRequestQueryString [("Action", Just "ListUsers"), ("Version", Just "2010-05-08")] initReq
+                { requestHeaders =
+                    [ ("X-Amz-Date", "20150830T123600Z"),
+                      ("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")]
+                }
+    return req
+
+targetRequest :: Request
+targetRequest = fromJust targetRequestM
 
 main :: IO ()
 main = hspec $ do
@@ -14,4 +31,4 @@ main = hspec $ do
       head [23 ..] `shouldBe` (23 :: Int)
 
     it "read content" $ do
-        (BS.readFile "data/example.txt") `shouldReturn`  ""
+        (BS.readFile "data/example.txt") `shouldReturn`  (canonicalRequest targetRequest "")
