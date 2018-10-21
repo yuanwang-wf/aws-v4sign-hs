e388711dfbaa6be404ca845018432ee33ae3fc18
Yuan Wang <yuan.w0811@gmail.com>
2018-10-20 20:34:12 -0600

    read content from file

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Data.ByteString           (ByteString)
-import           Data.Maybe
-import           Lib
-import Test.Hspec
+import qualified   Data.ByteString as BS
+import             Data.Maybe
+import             Lib
+import             Test.Hspec
 
 
 
@@ -12,3 +12,6 @@ main = hspec $ do
   describe "Prelude.head" $ do
     it "returns the first element of a list" $ do
       head [23 ..] `shouldBe` (23 :: Int)
+
+    it "read content" $ do
+        (BS.readFile "data/example.txt") `shouldReturn`  ""
